4d9241218b0c915fef6973416b07a2601fadaf75
Semmy Purewal <semmypurewal@gmail.com>
2013-09-23 09:42:16 -0700

    Add closure compiler, minify script and tests for minified version

---
+++ test/min/externs.js
@@ -0,0 +1,11 @@
+var clarinet = {
+  parser: function () {},
+  CParser: function () {},
+  CStream: function () {},
+  createStream: function () {},
+  MAX_BUFFER_LENGTH: 1,
+  DEBUG: false, 
+  INFO: false,
+  EVENTS: [],
+  STATE: []
+};
